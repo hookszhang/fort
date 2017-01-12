25fc69ed92ca7045909f71ee9388d71453d31582
hookszhang <hua.zhang@boyuanitsm.com>
2017-01-12 13:26:26 +0800

    Copyright 2016-2017 Shanghai Boyuan IT Services Ltd.

--- src/main/java/com/boyuanitsm/fort/service/MailService.java
+++ src/main/java/com/boyuanitsm/fort/service/MailService.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016-2017 Shanghai Boyuan IT Services Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.boyuanitsm.fort.service;
 
 import com.boyuanitsm.fort.config.JHipsterProperties;
@@ -30,7 +46,7 @@ import java.util.Locale;
 public class MailService {
 
     private final Logger log = LoggerFactory.getLogger(MailService.class);
-    
+
     private static final String USER = "user";
     private static final String BASE_URL = "baseUrl";
 
@@ -101,5 +117,5 @@ public class MailService {
         String subject = messageSource.getMessage("email.reset.title", null, locale);
         sendEmail(user.getEmail(), subject, content, false, true);
     }
-    
+
 }
